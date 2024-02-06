2a83da324f0cfa42a407cc206569da771940e419
veroniccaStrand <veronicca.strand@gmail.com>
2024-02-06 12:46:57 +0100

    server mongodb running

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import connectDB from './backend/config/db.js';
 const app = express();
-
+dotenv.config();
+connectDB();
 //routes
 import userRoutes from './backend/routes/userRoutes.js';
 app.use('/api/users', userRoutes);
