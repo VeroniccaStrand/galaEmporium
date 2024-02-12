54452c03d71210f956eb9f07ea0f26763c4d65e1
VeroniccaStrand <veronicca.strand@gmail.com>
2024-02-12 08:51:40 +0100

    ändrade felstavning

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(errorHandler);
+
 
 
 dotenv.config();
@@ -26,5 +26,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/tickets", ticketRoutes);
 app.use("/api/tickets/info",ticketRoutes);
+
+app.use(errorHandler);
 
 export default app;
