c32cc215c4f1861b60bc751356c3a2be4750961c
VeroniccaStrand <veronicca.strand@gmail.com>
2024-02-14 10:20:02 +0100

    changed name for css

--- frontend/pages/createEvent/createEvent.js
+++ frontend/pages/createEvent/createEvent.js
@@ -3,19 +3,19 @@ export default function createEventForm() {
   <section class='wrap'>
     <div class="create-event-form">
       <form id="createEventForm" onsubmit="submitCreateEventForm(); return false">
-        <h1>Create Event</h1>
+        <h1 class='create-event-title'>Create Event</h1>
         <p class='create__instructions'>
         Notera: Om det uppstår några problem under processen, 
         kommer felmeddelanden att visas för att hjälpa dig korrigera 
         eventuella fel. Tveka inte att kontakta support om du stöter på några problem.
         </p>
-        <label for="eventName">Event Name:</label>
+        <label class='create-label' for="eventName">Event Name:</label>
         <input type="text" id="eventName" name="eventName" required />
 
-        <label for="dateTime">Date and Time:</label>
+        <label class='create-label' for="dateTime">Date and Time:</label>
         <input type="datetime-local" id="dateTime" name="dateTime" required />
 
-        <label for="eventDescription">Event Description:</label>
+        <label class='create-label' for="eventDescription">Event Description:</label>
         <textarea
           id="eventDescription"
           name="eventDescription"
@@ -23,13 +23,13 @@ export default function createEventForm() {
           required
         ></textarea>
 
-        <label for="ticketCount">Number of Tickets:</label>
+        <label class='create-label' for="ticketCount">Number of Tickets:</label>
         <input type="number" id="ticketCount" name="ticketCount" required />
 
-        <label for="eventPrice">Event Price:</label>
+        <label class='create-label' for="eventPrice">Event Price:</label>
         <input type="number" id="eventPrice" name="eventPrice" required />
 
-        <label for="eventImage">Event Image:</label>
+        <label class='create-label' for="eventImage">Event Image:</label>
         <input type="file" id="eventImage" name="eventImage" accept="image/*" />
 
         <input type="submit" id="createEventButton" value="Create Event" />
