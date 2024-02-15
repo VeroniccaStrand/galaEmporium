6c19581dad0f5a332323821dbf3a7bc79bd73799
starkeOnline <starke.robin@outlook.com>
2024-02-15 13:25:36 +0100

    CSS styling

--- frontend/pages/dandelionJazzClub/dandelion.js
+++ frontend/pages/dandelionJazzClub/dandelion.js
@@ -14,10 +14,10 @@ function formatDateTime(dateTimeString) {
     return formattedDate;
 }
 
-export default async function nomads(clubId = '65cc8a73e536d3509bd17b59') {
+export default async function dandelion(clubId = '65cc8a73e536d3509bd17b59') {
     try {
         const response = await fetch(`http://localhost:3000/api/events/club/${clubId}`, {
-            method: 'GET', // Ändra metoden till GET eftersom du hämtar data
+            method: 'GET', 
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -27,7 +27,6 @@ export default async function nomads(clubId = '65cc8a73e536d3509bd17b59') {
 
         if (!response.ok) {
             console.error("Error fetching events:", response.statusText);
-            // Hantera felet efter behov
             return "Error fetching data from the database";
         }
 
@@ -38,48 +37,64 @@ export default async function nomads(clubId = '65cc8a73e536d3509bd17b59') {
                 const cardClass = index % 2 === 0 ? "even" : "odd";
 
                 return `
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Protest+Strike&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+
             <div class="event-card ${cardClass}">
-              <div class="event-info">
-                <p class='date-time'>${formatDateTime(event.dateTime)}</p>
-                <div class='event-text-wrap'>
-                  <h3 class='event-title'>${event.name}</h3>
-                  <p class='event-desc'>${event.desc}</p>
-                </div>
-                <div class="buy-wrap">
-                  <span class="price">Price: ${event.price}kr</span>
-                  <span class="tickets">Tickets left: ${event.tickets}</span>
-                  <button class='buy-btn'>Buy Ticket</button>
-                </div>
+              <div class="dandelionEventInfo">
+                <p class="dateTime">${formatDateTime(event.dateTime)}</p>
+
+                  <div id="dandelionEventTitle">${event.name}</div>
+                  <p class="dandelionEventDesc">${event.desc}</p>
+
+                    <span class="ticketInfo">
+                        Price: ${event.price}kr
+                        <br/>
+                        Tickets left: ${event.tickets}
+                        <br/>
+                        <a href="#köpatickets"> <img id="dandelionTicketBtn" src="pages/dandelionJazzClub/images/ticket.png" height="50px" width="139px"></a>
+                    </span>
+
               </div>
             </div>
           `;
             })
             .join("");
 
-        console.log(events);
         return `        
-        <div id="dandelionWrap">
+        <div id="dandelionWrapper">
 
             <div id="dandelionHeader">
-                <img id="clubLogo" src="pages/dandelionJazzClub/images/djc.png" alt="Dandelion club logo" height="200px" width="826px"></a>
+                <img id="clubLogo" src="pages/dandelionJazzClub/images/djc.png" alt="Dandelion club logo" height="200px" width="750px"></a>
             </div>
 
             <div id="dandelionNav">
-                <a href="#dandelionAboutHead"> <img class="dandelionAboutBtn" src="pages/dandelionJazzClub/images/about.png" width="150px"></a>
-                <a href="#dandelionCalendarHead"> <img class="dandelionCalendarBtn" src="pages/dandelionJazzClub/images/events.png" width="150px"></a>
-                <a href="#dandelionContactHead"> <img class="dandelionContactBtn" src="pages/dandelionJazzClub/images/contact.png" width="150px"></a>
+                <a href="#dandelionAboutHead"> <img src="pages/dandelionJazzClub/images/about.png" height="100px" width="181px"></a>
+                <a href="#dandelionCalendarHead"> <img src="pages/dandelionJazzClub/images/events.png" height="100px" width="201px"></a>
+                <a href="#dandelionContactHead"> <img  src="pages/dandelionJazzClub/images/contact.png" height="100px" width="220px"></a>
             </div>
 
             <div id="photoCollection">
-                <img src="pages/dandelionJazzClub/images/djc1.jpeg" alt="Club" height="100px" width="350px" ></a>
-                <img src="pages/dandelionJazzClub/images/djc2.png" alt="Artwork" height="100px" width="350px" ></a>
-                <img src="pages/dandelionJazzClub/images/djc3.jpg"alt="Performance" height="100px" width="350px" ></a>
+                <img class="dandelionPic" src="pages/dandelionJazzClub/images/djc1.jpeg" alt="Club" height="100px" width="350px" ></a>
+                <img class="dandelionPic" src="pages/dandelionJazzClub/images/djc2.png" alt="Artwork" height="100px" width="350px" ></a>
+                <img class="dandelionPic" src="pages/dandelionJazzClub/images/djc3.jpg"alt="Performance" height="100px" width="350px" ></a>
+                <img class="dandelionPic" src="pages/dandelionJazzClub/images/djc4.jpeg"alt="The bar" height="100px" width="350px" ></a>
+
             </div>
 
             <img class="dandelionAboutBtn" src="pages/dandelionJazzClub/images/about.png" width="150px">
                 <div id="dandelionAboutHead"></div>
 
-                <div id="dandelionAbout"></div>
+                <div id="dandelionAbout">
+                    <p>The Dandelion Jazz Club is a jazz club and restaurant located at 131 West 3rd Street in Greenwich Village, 
+                        New York City. The club's performance schedule features shows every evening at 8:00 pm and 10:30 pm and a 
+                        Sunday jazz brunch. The club has locations across the globe in New York, NY; Waikiki, Hawaii; Napa, CA; Tokyo, Japan; 
+                        Rio de Janeiro and São Paulo, Brazil; Milan, Italy; Beijing and Shanghai, China. 
+                        </p>
+
+                </div>
                 
 
             <img class="dandelionCalendarBtn" src="pages/dandelionJazzClub/images/events.png" width="150px">
@@ -92,10 +107,7 @@ export default async function nomads(clubId = '65cc8a73e536d3509bd17b59') {
             <img class="dandelionContactBtn" src="pages/dandelionJazzClub/images/contact.png" width="150px">
             <div id="dandelionContactHead"></div>
             <div id="dandelionContact">
-                <p>Namn: Bossman
-                    Tele:213
-                    Email: kfjlsnd
-                </p>
+                <img id="clubLogo" src="pages/dandelionJazzClub/images/djc.png" alt="Dandelion club logo" height="60px" width="250px"></a>
             </div>
 
         </div>
