d92b158ba85eb28e1022aa2800d142f47bbef6ec
starkeOnline <starke.robin@outlook.com>
2024-02-21 13:51:12 +0100

    Code cleaning

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-//const salt = "saltamera";
 
 // create
 export const createUser = async (req, res) => {
