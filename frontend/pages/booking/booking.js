90cf729826bbfbaad4be7736f8ad59a9ce60e272
Hoube14 <Hoube14@gmail.com>
2024-02-21 11:40:39 +0100

    added comments to the code in frontend

--- frontend/pages/booking/booking.js
+++ frontend/pages/booking/booking.js
@@ -1,23 +1,29 @@
+// Funktion för att köpa biljett till ett event
 export default async function buyTicket(eventId) {
+  // Skapa objekt med event-id
   const data = {
     eventId: eventId,
   };
 
   console.log(data);
   try {
+    // Skicka förfrågan för att köpa biljett till servern
     const result = await fetch("api/Tickets", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-      
     });
-    console.log(data)
+    console.log(data);
+
+    // Visa meddelande om att köpet lyckades
     alert("Purchase successful");
 
-    location.reload()
+    // Ladda om sidan efter att köpet är genomfört för att uppdatera biljettinformationen
+    location.reload();
   } catch (error) {
+    // Hantera eventuella fel vid köp av biljett
     console.error("Error fetching data:", error);
     return "Error fetching data from the database";
   }
